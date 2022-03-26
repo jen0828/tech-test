78b46a0a0b638ae8ddfc021d55e3e35a86d90710
Jenny Moon <jenny0moon@gmail.com>
2022-03-26 12:16:45 +0000

    Complete function: can print out top 5 frequent numbers

--- topFiveNum.js
+++ topFiveNum.js
@@ -1,10 +1,23 @@
 const topFiveFrequentNumbers = function (string, myNum = 5) {
   let new_array = string.split('\n');
+  let freq = {};
   let result = [];
 
   new_array.forEach((num) => {
-    result.push(num);
+    if (freq[num]) {
+      freq[num]++;
+    } else {
+      freq[num] = 1;
+    }
   });
+
+  let array = Object.entries(freq);
+
+  array.sort((a, b) => {
+    return b[1] - a[1];
+  });
+
+  array.slice(0, myNum).forEach((element) => result.push(element[0]));
 
   return result.join('\n');
 };
--- topFiveNum.test.js
+++ topFiveNum.test.js
@@ -7,7 +7,25 @@ describe('#topFiveFrequentNumbers', () => {
     });
 
     it('should print out 2 numbers in a string', () => {
-      expect(topFiveFrequentNumbers('2\n1', 5)).toBe('2\n1');
+      expect(topFiveFrequentNumbers('2\n1', 5)).toBe('1\n2');
+    });
+  });
+
+  describe(' When output is top five numbers', () => {
+    it('should print out all numbers in the original string', () => {
+      expect(topFiveFrequentNumbers('5\n4\n3\n2\n1', 5)).toBe('1\n2\n3\n4\n5');
+    });
+
+    it('should print out a new string of numbers in descending order', () => {
+      expect(topFiveFrequentNumbers('1\n2\n3\n4\n5\n5', 5)).toBe(
+        '5\n1\n2\n3\n4'
+      );
+    });
+
+    it('should print out a new string of numbers in descending order', () => {
+      expect(
+        topFiveFrequentNumbers('1\n2\n3\n4\n5\n5\n5\n5\n4\n4\n4\n4\n4', 5)
+      ).toEqual('4\n5\n1\n2\n3');
     });
   });
 });
