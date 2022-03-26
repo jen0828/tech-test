3ed8b6c161b81a8b8ad4b0b40d8c3c114878ab2d
Jenny Moon <jenny0moon@gmail.com>
2022-03-26 12:33:06 +0000

    Add function in main page

---
+++ main.js
@@ -0,0 +1,28 @@
+const topFiveFrequentNumbers = function (string, myNum = 5) {
+  let new_array = string.split('\n');
+  let freq = {};
+  let result = [];
+
+  if (string === '') {
+    throw new Error('File is empty!');
+  }
+
+  new_array.forEach((num) => {
+    if (freq[num]) {
+      freq[num]++;
+    } else {
+      freq[num] = 1;
+    }
+  });
+
+  let array = Object.entries(freq);
+
+  array.sort((a, b) => {
+    return b[1] - a[1];
+  });
+
+  array.slice(0, myNum).forEach((element) => result.push(element[0]));
+
+  return result.join('\n');
+};
+
